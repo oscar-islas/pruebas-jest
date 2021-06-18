189c74253c12974a9d940408545e345f36be2cee
oislasreyes <oislasreyes@gmail.com>
2021-06-17 21:19:56 -0500

    Template exercises

---
+++ exercises/first-exe.js
@@ -0,0 +1,20 @@
+//Escribe una función que tome un objeto con las propiedades number y percentage como argumentos
+//Regresa el porcentaje del numero dado
+
+//Ejemplo:
+/*
+Input: getPercent({number: 100, percentage: 50})
+Output: 50
+
+Input: getPercent({number: 777, percentage: 2})
+Output: 15.54
+
+Input: getPercent({number: 500, percentage: 99})
+Output: 495
+*/
+
+const getPercent = (opObj) => {
+    return 0;
+}
+
+module.exports = getPercent;
---
+++ exercises/second-exe.js
@@ -0,0 +1,18 @@
+//Escribe una función que tome un numero como argumento
+//Si el numero es un numero entero (sin decimales) regresa true
+//De lo contrario regresa false
+
+//Ejemplo:
+/*
+Input: isInteger(4)
+Output: true
+
+Input: isInteger(2.2)
+Output: false
+
+Input: isInteger(6)
+Output: true
+
+Input: isInteger('cadena')
+Output: true
+*/
